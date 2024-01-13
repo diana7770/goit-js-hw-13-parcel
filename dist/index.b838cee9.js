204c370cef2975801e5cd85dbaf21cbd0f9bbc65
diana7770 <20diane09@gmail.com>
2024-01-13 12:57:48 +0200

    deploy

---
+++ dist/index.b838cee9.js
@@ -0,0 +1,2 @@
+console.log(void alert(`\u{432}\u{456}\u{442}\u{430}\u{44E}! Тетяна \u{432}\u{438} \u{443}\u{441}\u{43F}\u{456}\u{448}\u{43D}\u{43E} \u{437}\u{430}\u{43B}\u{43E}\u{433}\u{456}\u{43D}\u{438}\u{43B}\u{438}\u{441}\u{44C})))`));
+//# sourceMappingURL=index.b838cee9.js.map
